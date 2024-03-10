64e394c245ca67f5487258001e0e2673d034c357
Meet Sonawane <51074036+meet447@users.noreply.github.com>
2024-03-10 16:25:36 +0530

    Create check_symmetric_or_not.c

---
+++ assignment/check_symmetric_or_not.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int r,c,a[3][3],t[3][3],flag=0;
+	clrscr();
+	printf("enter the elements of the matrix:\n");
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		scanf("%d",&a[r][c]);
+	}
+	printf("\n matrix is:\n");
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		printf("%d ",a[r][c]);
+		printf("\n");
+	}
+	printf("\ntranspose is:\n");
+	for(c=0;c<=2;c++)
+	{
+		for(r=0;r<=2;r++)
+		t[c][r]=a[r][c];
+	}
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		printf("%d ",t[r][c]);
+		printf("\n");
+	}
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		{
+			if(a[r][c]!=t[r][c])
+			{
+				flag=1;
+				break;
+			}
+		}
+	}
+	if(flag==1)
+	printf("\nMatrix is not Symmetric");
+	else
+	printf("\nMatrix is Symmetric");
+	getch();
+}
