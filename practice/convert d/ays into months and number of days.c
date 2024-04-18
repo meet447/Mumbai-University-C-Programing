7165a27614326324abd3675adf8c8aaa212dcc8b
Meet Sonawane <51074036+meet447@users.noreply.github.com>
2024-04-18 22:31:39 +0530

    Create ays into months and number of days.c

---
+++ practice/convert d/ays into months and number of days.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main ()
+{
+   int months, days ;
+   printf("Enter days") ;
+   
+   scanf("%d", &days) ;
+   
+   months = days / 30 ;
+   
+   days = days % 30 ;
+   
+   printf("Months = %d Days = %d", months, days) ;
+}
