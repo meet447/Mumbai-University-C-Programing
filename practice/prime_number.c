b778553ee1517386a133cfb572d618e111393226
Meet Sonawane <51074036+meet447@users.noreply.github.com>
2024-04-18 22:58:44 +0530

    Create prime_number.c

---
+++ practice/prime_number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main ()
+{
+   int a;
+   
+   a = 1;
+
+   if(a == 0 || a == 1)
+   {
+       printf("NOOO PRIME");
+   }
+   
+   else if(a % 2 != 0)
+   {
+       printf("PRIMEEE");
+   }
+   
+   else
+   {
+       printf("NO PRIMEEEEEEEEEEE");
+   }
+}
